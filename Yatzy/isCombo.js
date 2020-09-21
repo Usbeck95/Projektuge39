9bda3379ab53418c532db9cfb941e6f527b39121
Khava <khavushka@gmail.com>
2020-09-21 11:54:48 +0200

    fjernet sidste terning og tilføjet js kode

--- Yatzy/isCombo.js
+++ Yatzy/isCombo.js
@@ -1,11 +1,46 @@
+
+//Kontrollerer, om alle elementer i matrixen er ens
+function erEns(array) {
+    if (array.every((val, i ,arr)=> val ==
+    arr[0])){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//Sorterer array og tæller hvor mange terninger -1 er i en lige
+function howMany(array) {
+    let sorted = array.sort();
+    let length = 0;
+    for (var i = o; i < sorted.length - 1; i++) {
+        if(sorted[i + 1] - sorted[i] === 1){
+            length += 1;
+        }
+    }
+    return length;
+};
+
 // Beregner scoren for et fuldt hus
 function isCombo() {
       let score = 0;  
       let isCombo = roll;
-      let frekvens = {};
+      let frequency = {};
       let max = 0;
       let most;
       for(var v in isCombo) {
-          frekvens[isCombo[v]]
+        frequency[isCombo[v]]=
+          (frequency[isCombo[v]] || 0) + 1;
+          if (frequency[isCombo[v]] > max) {
+              max = frequency[isCombo[v]];
+              most = isCombo[v];
+          }
+          if(max === 3) {
+              isCombo = $.grep(isCombo, 
+            function(value) {
+                return value != most;
+            });
+            if []
+          }
       }
 }
