eb1841d7c29a5af9441af1c20b1851d56bcf183e
Khava <khavushka@gmail.com>
2020-09-21 11:28:45 +0200

    styling til terninger

--- Yatzy/isCombo.js
+++ Yatzy/isCombo.js
@@ -1,4 +1,11 @@
 // Beregner scoren for et fuldt hus
 function isCombo() {
-        
+      let score = 0;  
+      let isCombo = roll;
+      let frekvens = {};
+      let max = 0;
+      let most;
+      for(var v in isCombo) {
+          frekvens[isCombo[v]]
+      }
 }
